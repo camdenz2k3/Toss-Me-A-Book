80f9693c66bdbcba97934b45306b8c35a1b7b987
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-10-06 21:47:35 -0500

    creates and appends the elements needed for info on a random best seller book

--- Develop/Assets/javascript.js
+++ Develop/Assets/javascript.js
@@ -1,18 +1,60 @@
-var randomBtn = document.getElementById('random');
-var resultsCard = document.getElementById('card');
+var randomBtn = document.querySelector('.btn');
+var body = document.querySelector('body')
+
+var results = document.createElement('div');
+body.appendChild(results);
 
 var APIKey = 'wb2uvHErZufqaDA4aEQjDBE7jQBpEfkX';
 
-function getRandomBook() {
+function getRandomBook(event) {
+    event.preventDefault()
+
+    while (results.hasChildNodes()) {
+        results.removeChild(results.firstChild);
+    }
+
     fetch('https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=' + APIKey)
     .then(function(response) {
         return response.json();
     })
     .then(function(book) {
         console.log(book)
-        
-        
-        console.log(book.results.lists[i].books[i].title);
+        var bookList = book.results.lists
+        console.log(bookList)
+        var randomList = bookList[Math.floor(Math.random() * bookList.length)]
+        console.log(randomList)
+        console.log(randomList.books)
+        var randomBook = randomList.books[Math.floor(Math.random() * randomList.books.length)]
+        console.log(randomBook)
+
+        var img = document.createElement('img');
+        img.setAttribute('src', randomBook.book_image);
+        img.setAttribute('alt', 'Book image not found for' + randomBook.title);
+        results.appendChild(img)
+
+        var title = document.createElement('h2');
+        title.textContent = randomBook.title;
+        results.appendChild(title);
+
+        var author = document.createElement('h3');
+        author.textContent = randomBook.author;
+        results.appendChild(author);
+
+        var description = document.createElement('p');
+        description.textContent = randomBook.description;
+        results.appendChild(description);
+
+        var amazon = document.createElement('a');
+        amazon.textContent = 'Amazon URL';
+        amazon.setAttribute('href', randomBook.amazon_product_url);
+        amazon.target = '_blank';
+        results.appendChild(amazon);
+
+        var newBookBtn = document.createElement('button');
+        newBookBtn.textContent = 'Choose New Book';
+        results.appendChild(newBookBtn);
+
+        newBookBtn.addEventListener('click',getRandomBook)
 
     })
 }
